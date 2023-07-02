d583f68e408d4f53fa5609dc13097f71e7ff1984
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-02 14:23:59 +0300

    Add evaluation

--- src/com/hmail/katerynashpak/Greeting.java
+++ src/com/hmail/katerynashpak/Greeting.java
@@ -3,5 +3,6 @@ package com.hmail.katerynashpak;
 public class Greeting {
     public static void main(String[] args) {
         System.out.println("Hello Wold");
+        System.out.println("1 + 1 = " + (1 + 1));
     }
 }
