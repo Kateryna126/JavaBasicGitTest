6fdcccebc73531e72501505a7e3193284b0eca03
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-02 14:38:13 +0300

    add method

--- src/com/hmail/katerynashpak/Greeting.java
+++ src/com/hmail/katerynashpak/Greeting.java
@@ -2,6 +2,9 @@ package com.hmail.katerynashpak;
 
 public class Greeting {
     public static void main(String[] args) {
+        prettyMethod();
+    }
+    private static void prettyMethod() {
         System.out.println("Hello Wold");
         System.out.println("1 + 1 = " + (1 + 1));
     }
