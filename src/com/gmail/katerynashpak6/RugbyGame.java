1bef839ac8f58effa962e68f4621c7e835d1972a
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-05 13:08:49 +0300

    Added fillArrayWithRandomNumbers method

--- src/com/gmail/katerynashpak6/RugbyGame.java
+++ src/com/gmail/katerynashpak6/RugbyGame.java
@@ -7,5 +7,14 @@ public class RugbyGame {
         int[] allBlacksAges = new int[20];
         int[] wallabiesAges = new int[32];
 
+        fillArrayWithRandomNumbers(allBlacksAges, 18, 40);
+        fillArrayWithRandomNumbers(wallabiesAges, 18, 40);
+    }
+
+    public static void fillArrayWithRandomNumbers(int[] array, int min, int max) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(max - min + 1) + min;
+        }
     }
 }
