d3470f1d3bc24773d2657a7201a14ec1cc21cc46
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-05 13:06:20 +0300

    Create the RugbyGame class with the main method

---
+++ src/com/gmail/katerynashpak6/RugbyGame.java
@@ -0,0 +1,11 @@
+package com.gmail.katerynashpak6;
+
+import java.util.Random;
+
+public class RugbyGame {
+    public static void main(String[] args) {
+        int[] allBlacksAges = new int[20];
+        int[] wallabiesAges = new int[32];
+
+    }
+}
