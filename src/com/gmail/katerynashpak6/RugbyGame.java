572f0e4a38667b5c4afb90079934e3b73ad0d9c4
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-05 13:09:39 +0300

    Added printTeamAges method

--- src/com/gmail/katerynashpak6/RugbyGame.java
+++ src/com/gmail/katerynashpak6/RugbyGame.java
@@ -9,12 +9,24 @@ public class RugbyGame {
 
         fillArrayWithRandomNumbers(allBlacksAges, 18, 40);
         fillArrayWithRandomNumbers(wallabiesAges, 18, 40);
+
+        printTeamAges(allBlacksAges, "All Blacks");
+        System.out.println("\n");
+
+        printTeamAges(wallabiesAges, "Wallabies");
+        System.out.println("\n");
     }
 
     public static void fillArrayWithRandomNumbers(int[] array, int min, int max) {
         Random random = new Random();
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(max - min + 1) + min;
+        }
+    }
+    public static void printTeamAges(int[] teamAges, String teamName) {
+        System.out.println("Ages of players in " + teamName + ":");
+        for (int age : teamAges) {
+            System.out.print(age + " ");
         }
     }
 }
