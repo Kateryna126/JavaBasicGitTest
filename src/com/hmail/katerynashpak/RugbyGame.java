592328748fca949b79d8dcf70e9e5db3affe3e12
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-05 11:19:59 +0300

    Generate random ages for each player in both teams

--- src/com/hmail/katerynashpak/RugbyGame.java
+++ src/com/hmail/katerynashpak/RugbyGame.java
@@ -1,9 +1,15 @@
 package com.hmail.katerynashpak;
 
+import java.util.Random;
+
 public class RugbyGame {
     public static void main(String[] args) {
         int[] team1Ages = new int[25];
         int[] team2Ages = new int[25];
-
+        Random random = new Random();
+        for (int i = 0; i < 25; i++) {
+            team1Ages[i] = random.nextInt(23) + 18;
+            team2Ages[i] = random.nextInt(23) + 18;
+        }
     }
 }
