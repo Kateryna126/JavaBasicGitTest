00470ddd218d98f45911ad134a8d797bdb9cd5c8
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-05 10:59:21 +0300

    Create two arrays to store the ages of players in each team

---
+++ src/com/hmail/katerynashpak/RugbyGame.java
@@ -0,0 +1,9 @@
+package com.hmail.katerynashpak;
+
+public class RugbyGame {
+    public static void main(String[] args) {
+        int[] team1Ages = new int[25];
+        int[] team2Ages = new int[25];
+
+    }
+}
