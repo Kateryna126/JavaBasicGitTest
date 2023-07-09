65d1e52d7209df551bee1ffa90293802e09047e4
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-09 12:33:26 +0300

    added generateRandomNumbers method

--- src/com/gmail/katerynashpak/LotteryGame.java
+++ src/com/gmail/katerynashpak/LotteryGame.java
@@ -1,2 +1,24 @@
 package com.gmail.katerynashpak;
 
+
+import java.util.Random;
+
+public class LotteryGame {
+    public static void main(String[] args) {
+        int[] lotteryNumbers = generateRandomNumbers(7);
+        int[] playerNumbers = generateRandomNumbers(7);
+
+    }
+
+    public static int[] generateRandomNumbers(int length) {
+        int[] numbers = new int[length];
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            numbers[i] = random.nextInt(10);
+        }
+
+        return numbers;
+    }
+
+}
