b986d76c78d49903055dbf361ac373f9b404bccb
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-10 19:07:54 +0300

    added prompt user for number of rows and columns

--- src/com/gmail/katerynashpak/MatrixTranspose.java
+++ src/com/gmail/katerynashpak/MatrixTranspose.java
@@ -1,2 +1,16 @@
-package com.gmail.katerynashpak;public class MatrixTransponse {
+package com.gmail.katerynashpak;
+
+import java.util.Scanner;
+
+public class MatrixTranspose {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows (M): ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns (N): ");
+        int columns = scanner.nextInt();
+    }
 }
+
