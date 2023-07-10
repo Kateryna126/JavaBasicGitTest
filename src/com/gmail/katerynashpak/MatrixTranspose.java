6ffc4de15904e856c8927744727490906b719ef7
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-10 19:10:02 +0300

    added the readMatrix method

--- src/com/gmail/katerynashpak/MatrixTranspose.java
+++ src/com/gmail/katerynashpak/MatrixTranspose.java
@@ -11,6 +11,20 @@ public class MatrixTranspose {
 
         System.out.print("Enter the number of columns (N): ");
         int columns = scanner.nextInt();
+
+        int[][] matrix = readMatrix(scanner, rows, columns);
+    }
+    public static int[][] readMatrix(Scanner scanner, int rows, int columns) {
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter the elements of the matrix: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        return matrix;
     }
 }
 
