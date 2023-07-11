7d075b53fed2ef1fb6442b6b981ebbc9de21f925
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-11 15:46:53 +0300

    added main method with info about person

--- src/com/gmail/katerynashpak/Person.java
+++ src/com/gmail/katerynashpak/Person.java
@@ -16,4 +16,14 @@ public class Person {
     public String personInfo() {
         return "Call citizen " + name + " " + surname + " from the city of " + city + " at the number " + phoneNumber + ".";
     }
+
+    public static void main(String[] args) {
+        int counter = 1;
+        Person person1 = new Person("Kateryna", "Shpak", "Kyiv", "0679697540");
+        System.out.println(counter++ + "." + person1.personInfo());
+        Person person2 = new Person("Oleksandr", "Poltavets", "Lviv", "0679874560");
+        System.out.println(counter++ + "." + person2.personInfo());
+        Person person3 = new Person("Anna", "Zhumar", "Odesa", "0676525007");
+        System.out.println(counter++ + "." + person3.personInfo());
+    }
 }
