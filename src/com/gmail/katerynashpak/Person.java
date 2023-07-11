a3f9e99ee27de4e303df83ec884acb7dacb95eeb
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-11 15:45:21 +0300

    The code line for initializing the Person object has been updated

--- src/com/gmail/katerynashpak/Person.java
+++ src/com/gmail/katerynashpak/Person.java
@@ -6,4 +6,11 @@ public class Person {
     private String city;
     private String phoneNumber;
 
+    public Person(String name, String surname, String city, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+    }
+
 }
