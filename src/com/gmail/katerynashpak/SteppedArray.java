28e56a55fab5ced343851c96bccee851ae36f1c8
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-14 13:16:57 +0300

    Added user input for the number of rows and elements in a row

--- src/com/gmail/katerynashpak/SteppedArray.java
+++ src/com/gmail/katerynashpak/SteppedArray.java
@@ -5,6 +5,12 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class SteppedArray {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int numberOfRows = scanner.nextInt();
+        System.out.print("Enter the maximum number of elements in a row: ");
+        int maxElementsInRow = scanner.nextInt();
+    }
 }
 
