15134e14ad665870919a34444523e4b2539c9369
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 11:40:57 +0300

    Added methods for printing burger composition to the Burger class

--- src/com/gmail/katerynashpak/Burger.java
+++ src/com/gmail/katerynashpak/Burger.java
@@ -1,6 +1,11 @@
 package com.gmail.katerynashpak;
 
 public class Burger {
+    private String bun;
+    private String meat;
+    private String cheese;
+    private String greens;
+    private String mayo;
 }
 
 
