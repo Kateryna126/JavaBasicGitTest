8757be65a95ea5e8c1254df317e23c54ba28910a
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 15:57:31 +0300

    Added information about users

--- src/com/gmail/katerynashpak/Main.java
+++ src/com/gmail/katerynashpak/Main.java
@@ -1,5 +1,9 @@
 package com.gmail.katerynashpak;
 
 public class Main {
-
+    public static void main(String[] args) {
+        FitnessTracker anna = new FitnessTracker("Anna", 7, 6, 1990, "oksana@example.com", "+380501234567", "Shevchenko", 60.0, "110/70", 12000);
+        FitnessTracker taras = new FitnessTracker("Taras", 8, 5, 1985, "taras@example.com", "+380501234568", "Kovalenko", 80.0, "120/80", 5000);
+        FitnessTracker natalia = new FitnessTracker("Natalia", 9, 4, 2000, "natalia@example.com", "+380501234569", "Petrenko", 55.0, "120/90", 7000);
+    }
 }
