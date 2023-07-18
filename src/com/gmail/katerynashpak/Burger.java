131f7675919b0da243c46f98b307557c1c7cdbcb
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 11:42:23 +0300

    Added displayIngredients methods for print the ingredients of a burger

--- src/com/gmail/katerynashpak/Burger.java
+++ src/com/gmail/katerynashpak/Burger.java
@@ -6,6 +6,16 @@ public class Burger {
     private String cheese;
     private String greens;
     private String mayo;
+
+
+    private void displayIngredients() {
+        System.out.println("Bun: " + bun);
+        System.out.println("Meat: " + meat);
+        System.out.println("Cheese: " + cheese);
+        System.out.println("Greens: " + greens);
+        System.out.println("Mayo: " + mayo);
+        System.out.println();
+    }
 }
 
 
