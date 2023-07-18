38d9c06ddce08f94443828a19c61a525c657563a
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 15:49:54 +0300

    Added printAccountInfo method for FitnessTracker

--- src/com/gmail/katerynashpak/FitnessTracker.java
+++ src/com/gmail/katerynashpak/FitnessTracker.java
@@ -1,12 +1,12 @@
 package com.gmail.katerynashpak;
 
 public class FitnessTracker {
-    private String name;
-    private int dayOfBirth;
-    private int monthOfBirth;
-    private int yearOfBirth;
-    private String email;
-    private String phone;
+    private final String name;
+    private final int dayOfBirth;
+    private final int monthOfBirth;
+    private final int yearOfBirth;
+    private final String email;
+    private final String phone;
     private String surname;
     private double weight;
     private String bloodPressure;
@@ -91,6 +91,20 @@ public class FitnessTracker {
         int currentYear = 2023;
         age = currentYear - yearOfBirth;
     }
+
+    public void printAccountInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Last Name: " + surname);
+        System.out.println("Date of Birth: " + dayOfBirth + "/" + monthOfBirth + "/" + yearOfBirth);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Weight: " + weight);
+        System.out.println("Blood Pressure: " + bloodPressure);
+        System.out.println("Steps Per Day: " + steps);
+        System.out.println();
+    }
 }
+
 
 
