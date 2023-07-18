7ad5cd8b805d63fe37cea65b052adcb6ff20f319
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 15:41:38 +0300

    Added a constructor for FitnessTracker

--- src/com/gmail/katerynashpak/FitnessTracker.java
+++ src/com/gmail/katerynashpak/FitnessTracker.java
@@ -13,6 +13,19 @@ public class FitnessTracker {
     private int steps;
     private int age;
 
+    public FitnessTracker(String name, int birthDay, int birthMonth, int birthYear, String email, String phone, String surname, double weight, String bloodPressure, int steps) {
+        this.name = name;
+        this.dayOfBirth = birthDay;
+        this.monthOfBirth = birthMonth;
+        this.yearOfBirth = birthYear;
+        this.email = email;
+        this.phone = phone;
+        this.surname = surname;
+        this.weight = weight;
+        this.bloodPressure = bloodPressure;
+        this.steps = steps;
+        calculateAge();
+    }
 
     private void calculateAge() {
         int currentYear = 2023;
