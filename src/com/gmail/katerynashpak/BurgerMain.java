fd1c42344583ba4285c4e39e8c11d670be0063cf
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 11:54:59 +0300

    Added a combination of three burgers to display in the console

--- src/com/gmail/katerynashpak/BurgerMain.java
+++ src/com/gmail/katerynashpak/BurgerMain.java
@@ -1,4 +1,13 @@
 package com.gmail.katerynashpak;
 
 public class BurgerMain {
+    public static void main(String[] args) {
+        Burger[] burgers = new Burger[3];
+        burgers[0] = new Burger( "Sesame bun", "Beef patty", "Cheddar cheese", "Lettuce", "Mayonnaise");
+        burgers[1] = new Burger( "Whole wheat bun", "Turkey patty", "Swiss cheese", "Spinach");
+        burgers[2] = new Burger( "Brioche bun", "Double beef patty");
+
+    }
+
 }
+
