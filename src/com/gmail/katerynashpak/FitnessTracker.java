791a31cbf71106c8a7297f90788d506b23fbe67e
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 15:39:52 +0300

    added parameters that contain information about users

--- src/com/gmail/katerynashpak/FitnessTracker.java
+++ src/com/gmail/katerynashpak/FitnessTracker.java
@@ -1,9 +1,17 @@
 package com.gmail.katerynashpak;
 
-import java.time.LocalDate;
-import java.time.Period;
-
 public class FitnessTracker {
+    private String name;
+    private int dayOfBirth;
+    private int monthOfBirth;
+    private int yearOfBirth;
+    private String email;
+    private String phone;
+    private String surname;
+    private double weight;
+    private String bloodPressure;
+    private int steps;
+    private int age;
 
 }
 
