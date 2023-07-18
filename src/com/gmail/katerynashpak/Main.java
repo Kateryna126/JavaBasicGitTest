1d76efba0fa56ee9177ed309b107d86fba6d9149
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 15:59:10 +0300

    Added code to display information about 3 users in the console

--- src/com/gmail/katerynashpak/Main.java
+++ src/com/gmail/katerynashpak/Main.java
@@ -5,5 +5,10 @@ public class Main {
         FitnessTracker anna = new FitnessTracker("Anna", 7, 6, 1990, "oksana@example.com", "+380501234567", "Shevchenko", 60.0, "110/70", 12000);
         FitnessTracker taras = new FitnessTracker("Taras", 8, 5, 1985, "taras@example.com", "+380501234568", "Kovalenko", 80.0, "120/80", 5000);
         FitnessTracker natalia = new FitnessTracker("Natalia", 9, 4, 2000, "natalia@example.com", "+380501234569", "Petrenko", 55.0, "120/90", 7000);
+
+        FitnessTracker[] users = {anna, taras, natalia};
+        for (FitnessTracker user : users) {
+            user.printAccountInfo();
+        }
     }
 }
