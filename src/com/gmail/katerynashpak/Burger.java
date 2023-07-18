bd1c0bf10e20d7b4039bb64d357adc6205898834
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-18 11:44:42 +0300

    Added constructor for the Classic Burger with 5 arguments

--- src/com/gmail/katerynashpak/Burger.java
+++ src/com/gmail/katerynashpak/Burger.java
@@ -7,6 +7,15 @@ public class Burger {
     private String greens;
     private String mayo;
 
+    public Burger(String bun, String meat, String cheese, String greens, String mayo) {
+        System.out.println("Classic Burger Ingredients:");
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayo = mayo;
+        displayIngredients();
+    }
 
     private void displayIngredients() {
         System.out.println("Bun: " + bun);
