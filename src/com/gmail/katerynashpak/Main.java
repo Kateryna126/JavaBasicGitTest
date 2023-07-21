1fb59e08b594ba230ad4d45ac6590fea8003ba7c
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-21 17:46:55 +0300

    Change the coordinates of a point

--- src/com/gmail/katerynashpak/Main.java
+++ src/com/gmail/katerynashpak/Main.java
@@ -11,6 +11,11 @@ public class Main {
         int x2 = points[1].getX();
         int y2 = points[1].getY();
 
+        System.out.println("Point 1: (" + x1 + ", " + y1 + ")");
+        System.out.println("Point 2: (" + x2 + ", " + y2 + ")");
+
+        points[0].setCoordinates(5, 6);
+
 
     }
 
