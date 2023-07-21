7c00b0e0fb5f3829bf4c9b17f4d69256c1bd7b67
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-21 15:22:30 +0300

    Added public class Point with values

---
+++ src/com/gmail/katerynashpak/Point.java
@@ -0,0 +1,12 @@
+package com.gmail.katerynashpak;
+
+public class Point implements Cloneable {
+    private int x;
+    private int y;
+
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
