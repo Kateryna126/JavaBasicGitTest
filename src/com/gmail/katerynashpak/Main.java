8a9c2d0ffb0d5ed6a868ac5e7ae03ff62d7a4745
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-21 17:46:00 +0300

    Getting the coordinates of points

--- src/com/gmail/katerynashpak/Main.java
+++ src/com/gmail/katerynashpak/Main.java
@@ -2,8 +2,16 @@ package com.gmail.katerynashpak;
 
 public class Main {
     public static void main(String[] args) {
-        Point [] point = new Point[2];
-        point[0] = new Point(6,4);
-        point[1] = new Point(5,7);
+        Point[] points = new Point[2];
+        points[0] = new Point(3, 4);
+        points[1] = new Point(3, 4);
+
+        int x1 = points[0].getX();
+        int y1 = points[0].getY();
+        int x2 = points[1].getX();
+        int y2 = points[1].getY();
+
+
     }
+
 }
