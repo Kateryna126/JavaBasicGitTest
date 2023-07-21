039fbc75bb3da48e7748cb418ca91fd54bb6668f
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-21 16:48:41 +0300

    Added two objects of type Point in main class

---
+++ src/com/gmail/katerynashpak/Main.java
@@ -0,0 +1,9 @@
+package com.gmail.katerynashpak;
+
+public class Main {
+    public static void main(String[] args) {
+        Point [] point = new Point[2];
+        point[0] = new Point(6,4);
+        point[1] = new Point(5,7);
+    }
+}
