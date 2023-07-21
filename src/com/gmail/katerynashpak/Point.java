cd05b010014a8ec3667f42d6c74719f378c35cea
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-21 15:33:58 +0300

    Added a static method for calculating the distance between two points:

--- src/com/gmail/katerynashpak/Point.java
+++ src/com/gmail/katerynashpak/Point.java
@@ -4,7 +4,7 @@ public class Point implements Cloneable {
     private int x;
     private int y;
 
-    public Point (int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -17,15 +17,20 @@ public class Point implements Cloneable {
         return y;
     }
 
-    public void setCoordinates(int x, int y){
+    public void setCoordinates(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distanceToPoint(Point otherPoint){
+    public double distanceToPoint(Point otherPoint) {
         int deltaX = this.x - otherPoint.getX();
         int deltaY = this.y - otherPoint.getY();
         return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
     }
 
+    public static double distanceBetweenPoints(Point point1, Point point2) {
+        int deltaX = point1.getX() - point2.getX();
+        int deltaY = point1.getY() - point2.getY();
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
 }
