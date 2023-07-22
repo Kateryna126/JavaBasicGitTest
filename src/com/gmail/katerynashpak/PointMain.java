b779a845162cb07dc926518aa0de125054abf5fb
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-22 12:18:08 +0300

    added a for loop to traverse an array of points and display the coordinates of each point

--- src/com/gmail/katerynashpak/PointMain.java
+++ src/com/gmail/katerynashpak/PointMain.java
@@ -1,20 +1,18 @@
 package com.gmail.katerynashpak;
 
-public class Main {
+public class PointMain {
     public static void main(String[] args) {
         Point[] points = new Point[2];
-        points[0] = new Point(3, 4);
-        points[1] = new Point(3, 4);
+        points[0] = new Point(5, 4);
+        points[1] = points[0].clone();
 
-        int x1 = points[0].getX();
-        int y1 = points[0].getY();
-        int x2 = points[1].getX();
-        int y2 = points[1].getY();
+        for (int i = 0; i < points.length; i++) {
+            int x = points[i].getX();
+            int y = points[i].getY();
+            System.out.println("Point " + (i + 1) + ": (" + x + ", " + y + ")");
+        }
 
-        System.out.println("Point 1: (" + x1 + ", " + y1 + ")");
-        System.out.println("Point 2: (" + x2 + ", " + y2 + ")");
-
-        points[0].setCoordinates(5, 6);
+        points[0].setCoordinates(6, 2);
 
         double distance = points[0].distanceToPoint(points[1]);
         double distanceStatic = Point.distanceBetweenPoints(points[0], points[1]);
@@ -30,7 +28,7 @@ public class Main {
         String pointString = points[0].toString();
         System.out.println("Point 1 as string: " + pointString);
 
-        Point point3 = points[0].clone();
+        Point point3 = (Point)points[0].clone();
         System.out.println("Point 3: " + point3);
     }
 
