0ee7c63479300c9fe7b1a6cdc04397830f9613f7
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-23 21:23:36 +0300

    added two objects which are instances of the TemperatureConverter class

---
+++ src/com/gmail/katerynashpak/MainConverter.java
@@ -0,0 +1,10 @@
+package com.gmail.katerynashpak;
+
+public class MainConverter {
+    public static void main(String[] args) {
+        TemperatureConverter celsiusFahrenheitConverter = new CelsiusFahrenheitConverter();
+        TemperatureConverter celsiusKelvinConverter = new CelsiusKelvinConverter();
+
+
+    }
+}
