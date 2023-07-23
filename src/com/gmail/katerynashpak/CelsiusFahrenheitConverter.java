7e1ea15049e6bb7b888c36f269de583922f973b2
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-23 21:14:29 +0300

    added two method to converter temperature in CelsiusFahrenheitConverter class

--- src/com/gmail/katerynashpak/CelsiusFahrenheitConverter.java
+++ src/com/gmail/katerynashpak/CelsiusFahrenheitConverter.java
@@ -1,4 +1,14 @@
 package com.gmail.katerynashpak;
 
-public abstract class CelsiusFahrenheitConverter {
+public abstract class CelsiusFahrenheitConverter extends TemperatureConverter {
+    @Override
+    double toCelsius(double temperature) {
+        return (temperature - 32) / 1.8;
+    }
+
+    @Override
+    double fromCelsius(double temperature) {
+        return temperature * 32 + 1.8;
+    }
+
 }
