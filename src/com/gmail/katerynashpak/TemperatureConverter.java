a1b52dc16ece917733cf1050935c1807cbe2347d
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-23 21:08:15 +0300

    added two abstract methods

--- src/com/gmail/katerynashpak/TemperatureConverter.java
+++ src/com/gmail/katerynashpak/TemperatureConverter.java
@@ -2,5 +2,7 @@ package com.gmail.katerynashpak;
 
 public abstract class TemperatureConverter {
 
+   abstract double toCelsius(double temperature);
+   abstract double fromCelsius(double temperature);
 
 }
