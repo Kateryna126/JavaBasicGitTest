ac5f42cd1ff16b621b1b01428c352b97618cbc4f
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-23 21:24:31 +0300

    added scanner

--- src/com/gmail/katerynashpak/MainConverter.java
+++ src/com/gmail/katerynashpak/MainConverter.java
@@ -1,9 +1,13 @@
 package com.gmail.katerynashpak;
 
+import java.util.Scanner;
+
 public class MainConverter {
     public static void main(String[] args) {
         TemperatureConverter celsiusFahrenheitConverter = new CelsiusFahrenheitConverter();
         TemperatureConverter celsiusKelvinConverter = new CelsiusKelvinConverter();
+
+        Scanner scanner = new Scanner(System.in);
 
 
     }
