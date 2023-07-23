b8b6b8f4f8b6f2b78db99d17fdc6921908913f8c
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-23 21:29:53 +0300

    added program to prints the results of the conversions to the console.

--- src/com/gmail/katerynashpak/MainConverter.java
+++ src/com/gmail/katerynashpak/MainConverter.java
@@ -9,6 +9,21 @@ public class MainConverter {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter temperature in Celsius:");
+        double celsius = scanner.nextDouble();
+
+        System.out.println(celsius + "°C to Fahrenheit: " + celsiusFahrenheitConverter.fromCelsius(celsius));
+        System.out.println(celsius + "°C to Kelvin: " + celsiusKelvinConverter.fromCelsius(celsius));
+
+        System.out.println("Enter a temperature in Fahrenheit: ");
+        double fahrenheit = scanner.nextDouble();
+
+        System.out.println(fahrenheit + "°F to Celsius: " + celsiusFahrenheitConverter.toCelsius(fahrenheit));
+
+        System.out.println("Enter a temperature in Kelvin: ");
+        double kelvin = scanner.nextDouble();
+
+        System.out.println(kelvin + "K to Celsius: " + celsiusKelvinConverter.toCelsius(kelvin));
 
     }
 }
