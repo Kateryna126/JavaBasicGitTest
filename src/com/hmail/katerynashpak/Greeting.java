61deb4246c4fb483161406df91c5c6bb9ae20a11
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-26 11:53:47 +0300

    delete master

--- src/com/hmail/katerynashpak/Greeting.java
+++ src/com/hmail/katerynashpak/Greeting.java
@@ -4,6 +4,7 @@ public class Greeting {
     public static void main(String[] args) {
         prettyMethod();
     }
+
     private static void prettyMethod() {
         System.out.println("Hello Wold");
         System.out.println("1 + 1 = " + (1 + 1));
