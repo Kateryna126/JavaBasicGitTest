5b66c5629769b794cbe12eb9a7e07f1076ddfc95
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-27 18:24:13 +0300

    added testDistanceToPoint test

--- src/test/java/com/gmail/katerynashpak/TestPoint.java
+++ src/test/java/com/gmail/katerynashpak/TestPoint.java
@@ -1,7 +1,17 @@
 package com.gmail.katerynashpak;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestPoint {
 
-
+    @Test
+    public void testDistanceToPoint() {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(3, 4);
+        double distance = point1.distanceToPoint(point2);
+        assertEquals(5.0, distance, 0.001);
+    }
 }
 
