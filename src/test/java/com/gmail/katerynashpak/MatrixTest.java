72268d86cba96ffd0b9f8f2691cff65e15a03951
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-27 22:36:11 +0300

    added testIsSquare test

--- src/test/java/com/gmail/katerynashpak/MatrixTest.java
+++ src/test/java/com/gmail/katerynashpak/MatrixTest.java
@@ -1,8 +1,7 @@
 package com.gmail.katerynashpak;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MatrixTest {
     @Test
@@ -10,7 +9,13 @@ public class MatrixTest {
         double[] array = {1, 2, 3, 4};
         double result = Matrix.average(array);
         assertEquals(2.5,result,0.0);
-
     }
+    @Test
+    public void testIsSquare() {
+        int[][] squareMatrix = {{1, 2}, {3, 4}};
+        assertTrue(Matrix.isSquare(squareMatrix));
 
+        int[][] nonSquareMatrix = {{1, 2, 3}, {4, 5, 6}};
+        assertFalse(Matrix.isSquare(nonSquareMatrix));
+    }
 }
