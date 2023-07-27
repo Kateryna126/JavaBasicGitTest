dbda24b20be95a8eea6047645d7ee2604429cff7
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-27 22:16:39 +0300

    added average method

---
+++ src/main/java/com/gmail/katerynashpak/Matrix.java
@@ -0,0 +1,13 @@
+package com.gmail.katerynashpak;
+
+public class Matrix {
+    public static double average(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / array.length;
+    }
+
+
+}
