3af26e854b4cacb209db9ab9f66a70b9ce1b1de9
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-27 19:36:33 +0300

    added testEqualsReflexive test

--- src/test/java/com/gmail/katerynashpak/TestPoint.java
+++ src/test/java/com/gmail/katerynashpak/TestPoint.java
@@ -1,7 +1,7 @@
 package com.gmail.katerynashpak;
 
 import org.junit.jupiter.api.Test;
-
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestPoint {
@@ -15,12 +15,17 @@ public class TestPoint {
     }
 
     @Test
-    public void testDistanceBetweenPoints(){
-        Point point = new Point(0,0);
-        Point otherPoint = new Point(3,4);
-        double distance = point.distanceBetweenPoints(point,otherPoint);
+    public void testDistanceBetweenPoints() {
+        Point point = new Point(0, 0);
+        Point otherPoint = new Point(3, 4);
+        double distance = Point.distanceBetweenPoints(point, otherPoint);
         assertEquals(5.0, distance, 0.001);
     }
 
+    @Test
+    public void testEqualsReflexive() {
+        Point point1 = new Point(0, 0);
+        assertTrue(point1.equals(point1));
+    }
 }
 
