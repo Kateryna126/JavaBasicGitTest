a645b46eeef1df24eabb92db3a316cf51e6555fe
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-28 20:46:55 +0300

    change testDistanceToPoint & testDistanceBetweenPoints test

--- src/test/java/com/gmail/katerynashpak/TestPoint.java
+++ src/test/java/com/gmail/katerynashpak/TestPoint.java
@@ -1,43 +1,44 @@
 package com.gmail.katerynashpak;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPoint {
 
+    private Point point;
+    private static final double DELTA = 0.001;
+
+    @BeforeAll
+    public void setUp(){
+        point = new Point(0,0);
+    }
+
     @Test
     public void testDistanceToPoint() {
-        Point point = new Point(0, 0);
+
         Point otherPoint = new Point(3, 4);
+
         double distance = point.distanceToPoint(otherPoint);
-        assertEquals(5.0, distance, 0.001);
+
+        assertEquals(5.0, distance, DELTA);
     }
 
     @Test
     public void testDistanceBetweenPoints() {
-        Point point = new Point(0, 0);
+
         Point otherPoint = new Point(3, 4);
+
         double distance = Point.distanceBetweenPoints(point, otherPoint);
-        assertEquals(5.0, distance, 0.001);
-    }
 
-    @Test
-    public void testEqualsReflexive() {
-        Point point = new Point(0, 0);
-        assertTrue(point.equals(point));
-    }
-
-    @Test
-    public void testHashCodeReflexive() {
-        Point point1 = new Point(0, 0);
-        assertEquals(point1.hashCode(), point1.hashCode());
+        assertEquals(5.0, distance, DELTA);
     }
 
     @Test
     public void testEqualsNull() {
-        Point point = new Point(0, 0);
+
         assertFalse(point.equals(null));
     }
 
