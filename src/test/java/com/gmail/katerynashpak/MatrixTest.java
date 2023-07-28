91d6f49c53c561ee7e30398b473f8cc1f9942eb1
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-28 22:33:57 +0300

    added empty lines in tests added the test testNotSquare

--- src/test/java/com/gmail/katerynashpak/MatrixTest.java
+++ src/test/java/com/gmail/katerynashpak/MatrixTest.java
@@ -7,23 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MatrixTest {
     @Test
     public void testAverage() {
+
         double[] array = {1, 2, 3, 4};
+
         double result = Matrix.average(array);
+
         assertEquals(2.5, result, 0.0);
     }
 
     @Test
     public void testIsSquare() {
+
         int[][] squareMatrix = {{1, 2}, {3, 4}};
+
         assertTrue(Matrix.isSquare(squareMatrix));
+    }
+    @Test
+    public void testNotSquare() {
 
         int[][] nonSquareMatrix = {{1, 2, 3}, {4, 5, 6}};
+
         assertFalse(Matrix.isSquare(nonSquareMatrix));
     }
 
     @Test
     public void testAverageFromNull() {
+
         Assertions.assertThrows(IllegalArgumentException.class, () -> Matrix.average(null));
     }
+
+    @Test
+    public void testIsSquareFromNull() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Matrix.isSquare(null));
+    }
 }
-   
