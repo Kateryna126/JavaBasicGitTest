7626213484cd61963717ca0ce236ea7ed5536d90
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-29 13:49:53 +0300

    added while loop that allows the user to choose a drink from a list

--- src/main/java/com/gmail/katerynashpak/DrinkMain.java
+++ src/main/java/com/gmail/katerynashpak/DrinkMain.java
@@ -7,6 +7,16 @@ public class DrinkMain {
         Scanner scanner = new Scanner(System.in);
         DrinkCost.displayAvailableDrinks();
 
+        boolean choosingDrinks = true;
+        while (choosingDrinks) {
+            System.out.println("Please choose a drink (enter the name or 'done' to finish):");
+            String drinkChoice = scanner.nextLine().toUpperCase();
+
+            if (drinkChoice.equals("DONE")) {
+                choosingDrinks = false;
+            } else {
+                DrinkCost.DrinkMachine(Drink.valueOf(drinkChoice));
+            }
 
         }
         System.out.println("Total number of drinks: " + DrinkCost.totalDrinks);
