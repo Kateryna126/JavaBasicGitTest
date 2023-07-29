cc93fe7576a22f93624dffb41b35833b2fd1673e
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-29 13:04:32 +0300

    created class DrinkCost with constants for the cost of each drink

--- src/main/java/com/gmail/katerynashpak/Drink.java
+++ src/main/java/com/gmail/katerynashpak/Drink.java
@@ -3,3 +3,12 @@ package com.gmail.katerynashpak;
 enum Drink {
     COFFEE, TEA, LEMONADE, MOJITO, WATER, COCA_COLA
 }
+
+class DrinkCost{
+    static final double COFFEE_COST = 15.0;
+    static final double TEA_COST = 7.0;
+    static final double LEMONADE_COST = 8.0;
+    static final double MOJITO_COST = 20.0;
+    static final double WATER_COST = 5.0;
+    static final double COCA_COLA_COST = 9.0;
+}
