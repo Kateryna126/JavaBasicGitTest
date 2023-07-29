d3dd5084c81cddfb010938705bfdbc95a65a9c43
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-29 13:13:53 +0300

    added methods for making a drink and updating the total cost

--- src/main/java/com/gmail/katerynashpak/Drink.java
+++ src/main/java/com/gmail/katerynashpak/Drink.java
@@ -14,4 +14,35 @@ class DrinkCost{
 
     static int totalDrinks = 0;
     static double totalCost = 0;
+
+    static void DrinkMachine(Drink drink){
+        switch (drink){
+            case COFFEE:
+                System.out.println("Making coffee");
+                totalCost+= COFFEE_COST;
+                break;
+            case TEA:
+                System.out.println("Making tea");
+                totalCost += TEA_COST;
+                break;
+            case LEMONADE:
+                System.out.println("Making lemonade");
+                totalCost += LEMONADE_COST;
+                break;
+            case MOJITO:
+                System.out.println("Making mojito");
+                totalCost += MOJITO_COST;
+                break;
+            case WATER:
+                System.out.println("Pouring water");
+                totalCost += WATER_COST;
+                break;
+            case COCA_COLA:
+                System.out.println("Pouring Coca-Cola");
+                totalCost += COCA_COLA_COST;
+                break;
+        }
+        totalDrinks++;
+    }
+
 }
