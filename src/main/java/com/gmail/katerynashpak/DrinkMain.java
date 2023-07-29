7de00540a9ae7e19659da3f6c10bef8612d28a40
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-29 13:32:33 +0300

    added some drinks

--- src/main/java/com/gmail/katerynashpak/DrinkMain.java
+++ src/main/java/com/gmail/katerynashpak/DrinkMain.java
@@ -4,7 +4,9 @@ public class DrinkMain {
     public static void main(String[] args) {
         DrinkCost.displayAvailableDrinks();
 
-
+        DrinkCost.DrinkMachine(Drink.COFFEE);
+        //DrinkCost.DrinkMachine(Drink.WATER);
+        DrinkCost.DrinkMachine(Drink.MOJITO);
     }
 }
 
