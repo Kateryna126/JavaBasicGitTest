d1c126d0f25664bfa2f7ae7b72e4d2291aff338c
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-29 12:21:04 +0300

    create public class Main with instances of the Androids and IPhones classes

---
+++ src/main/java/com/gmail/katerynashpak/Main.java
@@ -0,0 +1,10 @@
+package com.gmail.katerynashpak;
+
+public class Main {
+    public static void main(String[] args) {
+        Smartphones android = new Androids();
+        Smartphones iphone = new IPhones();
+
+
+    }
+}
