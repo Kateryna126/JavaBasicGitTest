5c5471b0b43f5f9c0f6b818bc4596152a4994f8c
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-07-29 12:34:53 +0300

    create for loop to call all instance in the array

--- src/main/java/com/gmail/katerynashpak/Main.java
+++ src/main/java/com/gmail/katerynashpak/Main.java
@@ -2,8 +2,15 @@ package com.gmail.katerynashpak;
 
 public class Main {
     public static void main(String[] args) {
-        Smartphones android = new Androids();
-        Smartphones iphone = new IPhones();
+        Smartphones[] phones = new Smartphones[2];
+        phones[0] = new Androids();
+        phones[1] = new IPhones();
+
+        for (Smartphones phone : phones) {
+            phone.call();
+            phone.sms();
+            phone.internet();
+        }
 
 
     }
