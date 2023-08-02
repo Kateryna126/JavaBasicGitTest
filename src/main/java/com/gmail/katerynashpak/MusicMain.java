89e0cf7c7f2366af7cd27e00ef0645b6997974f5
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-08-02 10:45:50 +0300

    created music bands for each style & Called the playMusic() method on all MusicStyles

--- src/main/java/com/gmail/katerynashpak/MusicMain.java
+++ src/main/java/com/gmail/katerynashpak/MusicMain.java
@@ -2,6 +2,9 @@ package com.gmail.katerynashpak;
 
 public class MusicMain {
     public static void main(String[] args) {
-
+        MusicStyles[] bands = {new MusicStyles.PopMusic(), new MusicStyles.RockMusic(), new MusicStyles.ClassicMusic()};
+        for (MusicStyles band : bands) {
+            band.playMusic();
+        }
     }
 }
