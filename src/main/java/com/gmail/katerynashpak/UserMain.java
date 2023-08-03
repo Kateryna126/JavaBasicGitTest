e8df9813f09d9ea918fab0f28fd57022cef9bf63
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-08-03 18:50:41 +0300

    Declared Scanner object and initialize errorCount variable

---
+++ src/main/java/com/gmail/katerynashpak/UserMain.java
@@ -0,0 +1,12 @@
+package com.gmail.katerynashpak;
+
+import java.util.Scanner;
+
+public class UserMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int errorCount = 0;
+
+    }
+}
