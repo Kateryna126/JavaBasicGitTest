bddb535b9780cfa4ab28d6c3ba259f581b650f72
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-08-03 17:18:52 +0300

    make MusicStyles abstract & fix

--- src/main/java/com/gmail/katerynashpak/MusicMain.java
+++ src/main/java/com/gmail/katerynashpak/MusicMain.java
@@ -2,7 +2,11 @@ package com.gmail.katerynashpak;
 
 public class MusicMain {
     public static void main(String[] args) {
-        MusicStyles[] bands = {new MusicStyles.PopMusic(), new MusicStyles.RockMusic(), new MusicStyles.ClassicMusic()};
+        MusicStyles[] bands = new MusicStyles[]{
+                new PopMusic(),
+                new RockMusic(),
+                new ClassicMusic()};
+
         for (MusicStyles band : bands) {
             band.playMusic();
         }
--- src/main/java/com/gmail/katerynashpak/MusicStyles.java
+++ src/main/java/com/gmail/katerynashpak/MusicStyles.java
@@ -1,27 +1,26 @@
 package com.gmail.katerynashpak;
 
-public class MusicStyles {
+public abstract class MusicStyles {
+    public abstract void playMusic();
+}
+
+class PopMusic extends MusicStyles {
+    @Override
     public void playMusic() {
+        System.out.println("Playing pop music");
     }
+}
 
-    static class PopMusic extends MusicStyles {
-        @Override
-        public void playMusic(){
-            System.out.println("Playing pop music");
-        }
+class RockMusic extends MusicStyles {
+    @Override
+    public void playMusic() {
+        System.out.println("Playing rock music");
     }
+}
 
-    static class RockMusic extends MusicStyles{
-        @Override
-        public void playMusic(){
-            System.out.println("Playing rock music");
-        }
-    }
-
-    static class ClassicMusic extends MusicStyles{
-        @Override
-        public void playMusic(){
-            System.out.println("Playing classic music");
-        }
+class ClassicMusic extends MusicStyles {
+    @Override
+    public void playMusic() {
+        System.out.println("Playing classic music");
     }
 }
