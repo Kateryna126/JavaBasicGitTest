8569c320c97c22ef08c26d481eaec1df2722ac46
Kateryna Shpak <KaterynaShpak6@gmail.com>
2023-08-03 18:41:41 +0300

    added constructor for the User class that takes 3 parameters

--- src/main/java/com/gmail/katerynashpak/User.java
+++ src/main/java/com/gmail/katerynashpak/User.java
@@ -4,16 +4,28 @@ public class User {
     private String login;
     private String password;
 
-}
-
-class WrongLoginException extends Exception {
-    public WrongLoginException(String message) {
-        super(message);
+    public User(String login, String password, String confirmPassword)
+            throws WrongPasswordException, WrongLoginException {
+        if (login.length() < 20 || !login.matches("[a-zA-Z]")) {
+            throw new WrongLoginException("Login must be no more than 20 characters and contain only letters of the English alphabet.");
+        }
+        if (!password.matches("(?=.*[0-9])(?=.*[a-zA-Z]).{6,25}") || !password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Password must contain at least 1 letter and 1 digit and must be between 6 and 25 characters long.");
+        }
+        if(!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Passwords do not match.");
+        }
     }
-}
 
-class WrongPasswordException extends Exception{
-    public WrongPasswordException(String message){
-        super(message);
+    class WrongLoginException extends Exception {
+        public WrongLoginException(String message) {
+            super(message);
+        }
+    }
+
+    class WrongPasswordException extends Exception {
+        public WrongPasswordException(String message) {
+            super(message);
+        }
     }
 }
